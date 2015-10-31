a916cfc816411791a4af632bf18400aa0768c307
Thalmann <mr.thalmann@gmail.com>
2015-10-31 14:53:16 +0100

    Added game lobby class

---
+++ lobby.py
@@ -0,0 +1,21 @@
+from game_info import GameInfo
+
+class Lobby:
+    'A lobby of players before actually starting the game managed by a host.'
+
+    def __init__(self, host):
+        _players = list()
+        self.host = host
+        self.players = players
+
+    def join(player):
+        players.append(player)
+
+    def kick(player):
+        players.remove(player)
+
+    def leave(player):
+        players.remove(player)
+
+    def start_game():
+        return GameInfo()
