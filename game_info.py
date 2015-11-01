9913e6492ebc063b9a10026d2e0887963694b735
Thalmann <mr.thalmann@gmail.com>
2015-11-01 15:13:28 +0100

    Added time and start game to game info

--- game_info.py
+++ game_info.py
@@ -1,11 +1,27 @@
 import random
+import time
 
 class GameInfo:
     'Class that contains the game state.'
 
-    def __init__(self):
+    def __init__(self, host, players):
+        self.host = host
+        players.append(host)
+        self.players = players
         self.location = get_location()
+        self.time = get_time_in_minutes()
 
     def get_location():
         locations = ['beach', 'others']
         return random.choice(locations)
+
+    def get_time_in_minutes(players):
+        if len(players) > 4:
+            return 8
+        return 7
+
+    def start_game():
+        while self.time:
+            minutes, seconds = divmod(t, 60)
+            time.sleep(1)
+            self.time -= 1
