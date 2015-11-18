bb9f757e582a2adfe991de8993cd5add3e79780e
Thalmann <mr.thalmann@gmail.com>
2015-11-18 20:33:57 +0100

    Added basic navigation templates

--- spyfall.py
+++ spyfall.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'What up!'
+    return render_template('index.html')
+
+@app.route('/games')
+def games():
+    return render_template('games.html')
+
+@app.route('/new_game.html')
+def new_game():
+    return render_template('new_game.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
