8f43285701e47bb93af21307ee5c66c51e344255
Thalmann <mr.thalmann@gmail.com>
2015-11-22 17:24:37 +0100

    Added safety for form input

--- spyfall.py
+++ spyfall.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask_wtf import Form
+from wtforms.fields import SubmitField, StringField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+class StartGameForm(Form):
+    """Simple form when creating a new game."""
+    
+    text = StringField('Name:')
+    submit = SubmitField('Start Game')
 
 @app.route('/')
 @app.route('/index')
@@ -15,10 +25,11 @@ def games():
 
 @app.route('/new_game', methods=['GET', 'POST'])
 def new_game():
-    if request.method == 'POST':
-        if request.form['game_name'] != '':
-            return render_template('game.html', game_name = request.form['game_name'])
-    return render_template('new_game.html')
+    form = StartGameForm()
+    if form.validate_on_submit():
+        if form.text.data != '':
+            return render_template('game.html', game_name = form.text.data)
+    return render_template('new_game.html', form = form)
 
 @app.route('/game')
 def game():
